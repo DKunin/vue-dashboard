3158381e462846b581d93ae370be55153da6448f
Dmitry Kunin <dkunin@avito.ru>
2017-04-04 08:44:04 +0300

    master:Minor adjustments

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -4,7 +4,6 @@
         <div class="flex-container">
             <section class="flex-section">
                 <tasksList v-visibilityUpdate search="http://192.168.99.100:4747/api/search?jql=assignee%20=%20currentUser()%20AND%20resolution%20=%20Unresolved%20order%20by%20updated%20DESC"/>
-                <newsList v-visibilityUpdate :search="'https://newsapi.org/v1/articles?source=newsweek&sortBy=latest&apiKey=' + state.newsApi"/>
             </section>
             <section class="flex-section">
                 <stashList v-visibilityUpdate :user="state.stashUserName" :hideWips="state.hideWips" />
@@ -16,9 +15,10 @@
             </section>
         </div>
     `;
-    fetch('./dashboard.html').then(result => result.text()).then(result => {
-        template = result;
-    });
+
+    // fetch('./dashboard.html').then(result => result.text()).then(result => {
+    //     template = result;
+    // });
 
     root.dashboard = {
         template,
