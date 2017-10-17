5b0f7c61c764a815579dcb33604c425c4383611b
Dmitry Kunin <dkunin@avito.ru>
2017-10-17 08:48:25 +0300

    Minor fiddle

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ Object.defineProperty(Vue.prototype, '$localIp', {
     value: 'http://127.0.0.1'
 });
 Object.defineProperty(Vue.prototype, '$localDockerIp', {
-    value: 'http://192.168.64.2'
+    value: 'http://192.168.64.3'
 });
 
 Vue.component('githubIssues', githubIssues);
