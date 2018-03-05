00883e5ed550359ab9ed4a45de3a21700c15d93c
Dmitry Kunin <dkunin@avito.ru>
2018-03-05 10:09:16 +0300

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -13,10 +13,10 @@ import searchIcon from './searchIcon.js';
 import app from './app.js';
 
 Object.defineProperty(Vue.prototype, '$localIp', {
-    value: 'http://10.10.10.23'
+    value: 'http://10.10.10.35'
 });
 Object.defineProperty(Vue.prototype, '$localDockerIp', {
-    value: 'http://10.10.10.23'
+    value: 'http://10.10.10.35'
 });
 
 Vue.component('githubIssues', githubIssues);
